d0c2ca3d4fd1a6c6f16d8f15e547480028de70f5
jiffylubeyou <kennyspaz@hotmail.com>
2021-09-12 18:01:22 -0600

    lots of changes

--- src/spell/Node.java
+++ src/spell/Node.java
@@ -1,18 +1,26 @@
 package spell;
 
 public class Node implements INode {
+    private int counter;
+    private Node[] children = new Node[26];
+
+    public Node()
+    {
+        counter = 0;
+    }
+
     @Override
     public int getValue() {
-        return 0;
+        return counter;
     }
 
     @Override
     public void incrementValue() {
-
+        counter = counter + 1;
     }
 
     @Override
-    public INode[] getChildren() {
-        return new INode[0];
+    public Node[] getChildren() {
+        return children;
     }
 }
--- src/spell/Trie.java
+++ src/spell/Trie.java
@@ -1,6 +1,11 @@
 package spell;
 
 public class Trie implements  ITrie {
+
+    private Node root;
+    private int wordCount;
+    private int nodeCount;
+
     @Override
     public void add(String word) {
 
@@ -13,11 +18,92 @@ public class Trie implements  ITrie {
 
     @Override
     public int getWordCount() {
-        return 0;
+        return wordCount;
     }
 
     @Override
     public int getNodeCount() {
-        return 0;
+        return nodeCount;
+    }
+
+    @Override
+    public String toString()
+    {
+        StringBuilder curWord = new StringBuilder();
+        StringBuilder output = new StringBuilder();
+        toString_Helper(root, curWord, output);
+        return output.toString();
+    }
+
+    private void toString_Helper(Node n, StringBuilder curWord, StringBuilder output)
+    {
+        if (n.getValue() > 0)
+        {
+            output.append(curWord.toString());
+            output.append("\n");
+        }
+        for (int i = 0; i < n.getChildren().length; ++i)
+        {
+            Node child = n.getChildren()[i];
+            if (child != null) {
+
+                char childLetter = (char)('a' + i);
+                curWord.append(childLetter);
+
+                toString_Helper(child, curWord, output);
+
+                curWord.deleteCharAt(curWord.length() - 1);
+            }
+        }
+    }
+
+    @Override
+    public boolean equals (Object o)
+    {
+        if (o == null)
+        {
+            return false;
+        }
+        if (this == o)
+        {
+            return true;
+        }
+        if (this.getClass() != o.getClass())
+        {
+            return false;
+        }
+        Trie d = (Trie) o;
+        if ((this.getWordCount() != d.getWordCount()) || (this.getNodeCount() != d.getNodeCount()))
+        {
+            return false;
+        }
+        return equals_Helper(this.root, d.root);
+    }
+
+    private boolean equals_Helper (Node n1, Node n2)
+    {
+        if (n1.getValue() != n2.getValue())
+        {
+            return false;
+        }
+        for (int i = 0; i < n1.getChildren().length; ++i)
+        {
+            if ((n1.getChildren()[i] == null) && n2.getChildren()[i] != null)
+            {
+                return false;
+            }
+            if ((n1.getChildren()[i] != null) && n2.getChildren()[i] == null)
+            {
+                return false;
+            }
+        }
+        for (int i = 0; i < n1.getChildren().length; ++i)
+        {
+            if (n1.getChildren()[i] != null && n2.getChildren()[i] != null)
+            {
+                equals_Helper(n1.getChildren()[i], n2.getChildren()[i]);
+            }
+        }
+        return true;
     }
 }
