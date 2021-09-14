6faa766395270179b71ed66126e6cfea90c22718
Kenton Carl Strong <kstrong3@carolina.cs.byu.edu>
2021-09-14 13:57:02 -0600

    Closer to being done with spellCorrector class

--- src/spell/SpellCorrector.java
+++ src/spell/SpellCorrector.java
@@ -1,17 +1,36 @@
 package spell;
 
+import com.sun.source.tree.Tree;
+
 import java.io.IOException;
+import java.util.Scanner;
+import java.io.File;
+import java.util.*;
 
 public class SpellCorrector implements ISpellCorrector {
+    Trie myTrie = new Trie();
+
     @Override
     public void useDictionary(String dictionaryFileName) throws IOException {
-        //popuate trie
+        File file = new File(dictionaryFileName);
+        Scanner scanner = new Scanner(file);
+
+        //populate my trie
+        while (scanner.hasNext())
+        {
+            String str = scanner.next();
+            myTrie.add(str);
+        }
     }
 
     @Override
     public String suggestSimilarWord(String inputWord) {
         //make set edit d 1 with set
-        //make set
+        TreeMap<String, Integer> deletionsMap = getPossibleDeletions(inputWord);
+        TreeMap<String, Integer> transpositionsMap = getPossibleTranspositions(inputWord);
+        TreeMap<String, Integer> alterationsMap = getPossibleAlterations(inputWord);
+        TreeMap<String, Integer> InsertionsMap = getPossibleInsertions(inputWord);
+
         //search trie for each word and add to another found set
         //1. if only one is found, return
         //return frequency of each and highest returned
@@ -19,4 +38,79 @@ public class SpellCorrector implements ISpellCorrector {
         //if none are found, call helper functions on all edit d 1 words
         return null;
     }
+
+    private TreeMap<String, Integer> getPossibleDeletions (String word)
+    {
+        TreeMap<String, Integer> myMap = new TreeMap<String, Integer>();
+        for (int i = 0; i < word.length(); ++i)
+        {
+            StringBuilder stringbuilder = new StringBuilder(word);
+            String newWord = stringbuilder.deleteCharAt(i).toString();
+            Node myNode = myTrie.find(newWord);
+            if (myNode != null)
+            {
+                myMap.put(newWord, myNode.getValue());
+            }
+        }
+        return myMap;
+    }
+
+    private TreeMap<String, Integer> getPossibleTranspositions (String word)
+    {
+        TreeMap<String, Integer> myMap = new TreeMap<String, Integer>();
+        for (int i = 0; i < word.length() - 1; ++i)
+        {
+            StringBuilder stringbuilder = new StringBuilder(word);
+            char firstChar = stringbuilder.charAt(i);
+            stringbuilder.deleteCharAt(i);
+            stringbuilder.insert((i+1), firstChar);
+            String newWord = stringbuilder.toString();
+            Node myNode = myTrie.find(newWord);
+            if (myNode != null)
+            {
+                myMap.put(newWord, myNode.getValue());
+            }
+        }
+        return myMap;
+    }
+
+    private TreeMap<String, Integer> getPossibleAlterations (String word)
+    {
+        TreeMap<String, Integer> myMap = new TreeMap<String, Integer>();
+        for (int i = 0; i < word.length(); ++i) {
+            for (int j = 0; j < 26; ++j)
+            {
+                StringBuilder stringbuilder = new StringBuilder(word);
+                char myChar = (char)('a' + j);
+                stringbuilder.setCharAt(i,myChar);
+                String newWord = stringbuilder.toString();
+                Node myNode = myTrie.find(newWord);
+                if (myNode != null)
+                {
+                    myMap.put(newWord, myNode.getValue());
+                }
+            }
+        }
+        return myMap;
+    }
+
+    private TreeMap<String, Integer> getPossibleInsertions (String word)
+    {
+        TreeMap<String, Integer> myMap = new TreeMap<String, Integer>();
+        for (int i = 0; i < word.length() + 1; ++i) {
+            for (int j = 0; j < 26; ++j) {
+                StringBuilder stringbuilder = new StringBuilder(word);
+                char myChar = (char)('a' + j);
+                stringbuilder.insert(i, myChar);
+                String newWord = stringbuilder.toString();
+                Node myNode = myTrie.find(newWord);
+                if (myNode != null)
+                {
+                    myMap.put(newWord, myNode.getValue());
+                }
+            }
+        }
+        return myMap;
+    }
+
 }
